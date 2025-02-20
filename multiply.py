bca4b8396a4db9702f2b99398e74c7a547d03137
mshep8 <marycatherineshepherd@gmail.com>
2025-02-20 10:27:59 -0700

    Created a function to multiply two numbers
    Takes two numbers and multiplies those numbers

---
+++ multiply.py
@@ -0,0 +1,8 @@
+# Mary Catherine
+# 2/20/2025
+# Take input and multiply it
+
+# Takes two numbers and returns the product
+def multiplyTwoNumbers(iNum1,iNum2):
+    result = iNum1*iNum2
+    return result
